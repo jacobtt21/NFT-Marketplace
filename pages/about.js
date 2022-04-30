3c48608dee47931edd459ffa7bb41a981868eecf
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-04-29 21:59:52 -0500

    working s/[id]

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,47 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-        <h1>Under Construction</h1>
+    <div className='landing'>
+      <h1>Confused about this whole thing?</h1>
+      <br />
+      <h2>Don't worry, we're here to explain...</h2>
+      <br />
+      <br />
+      <div className='explain'>
+      <h3>What is Oustro?</h3>
+      </div>
+      <div className='footer'>
+        <p>2023 Oustro Inc. v1.0.0-beta</p>
+      </div>
+      <style jsx>{`
+        h1 {
+          font-size: 50px;
+        }
+        h2 {
+          font-size: 30px;
+        }
+        .landing {
+          text-align:center;
+        }
+        .explain {
+          text-align:left;
+        }
+
+        .dis {
+          margin-top: 9px;
+          margin-bottom: 9px;
+          font-size: 11px;
+        }
+
+        .footer {
+          padding:15px;
+          border-top:1px solid #f0f0f0;
+          margin:240px auto 10px;
+          text-align:center;
+          font-size:15px
+        }
+      `}</style>
+    </div>
     </>
   );
 }
--- pages/s/[id].js
+++ pages/s/[id].js
@@ -1,11 +1,113 @@
-import { Icon, MonochromeIcons, TextField, CallToAction, Input } from '@magiclabs/ui';
-import Typical from 'react-typical'
-import Link from "next/link";
+import React, { useEffect, useContext, useState } from 'react';
+import { useRouter } from 'next/router'
+import { web3 } from '../../lib/magic';
+import { UserContext } from '../../lib/UserContext';
+import { abi } from '../../contracts/abi';
+import Loading from '../../components/Loading';
+import { TextField, CallToAction, useToast, TextButton } from '@magiclabs/ui';
+import Link from 'next/link'
 
-export default function About() {
+
+export default function Index() {
+  const [user] = useContext(UserContext);
+  const [disabled, setDisabled] = useState(false);
+  const router = useRouter();
+  const [theNFT, setTheNFT] = useState();
+  const [theData, setTheData] = useState();
+  const [newRating, setNewRating] = useState();
+  const [msg, setMsg] = useState(false);
+  const [msg1, setMsg1] = useState(false);
+
+  const contractAddress = process.env.NEXT_PUBLIC_COLLECTION_ADDRESS;
+  const contract = new web3.eth.Contract(abi, contractAddress);
+
+  useEffect(() => {
+    if (user) return;
+    getMyNFT();
+  }, [user]);
+
+  const getMyNFT = async () => {
+    
+    const nft = await contract.methods.getNFTbyId(parseInt(router.query.id)).call();
+    const response = await fetch(nft[0]);
+    const data = await response.json();
+    setTheNFT(data);
+    setTheData(nft);
+  };
+
   return (
-    <>
-        <h1>Under Construction</h1>
-    </>
-  );
+    <div>
+        {theData ? (
+            <>
+                <div className="mint-container">
+                <Link href="/login">
+                  
+                <CallToAction
+                  color="primary"
+                  >
+                  Take a closer look &rarr;
+                  </CallToAction>
+                
+                    </Link>
+                    <h1>{theNFT.name}</h1>
+                    <h3>Price: {web3.utils.fromWei(theData.price)} rETH</h3>
+                        <img
+                            src={theNFT.image}
+                            width={300}
+                            className="nft-img"
+                            onError={(e) => (e.target.src = '/fallback.jpeg')}
+                        />
+                    <CallToAction
+                    color="primary"
+                    size="sm"
+                    outline="none"
+                    >
+                      5 / 5 Rating
+                    </CallToAction>  
+                    <div className='name'>
+                      <h3>by {theNFT.creator.substring(0, 6)}..{theNFT.creator.substring(38)}</h3> 
+                    </div>
+                     
+                </div>
+                <style>{`
+                    h1 {
+                    font-weight: bold;
+                    font-size: 28px;
+                    margin: 20px;
+                    min-height: 28px;
+                    }
+                
+                    .mint-container {
+                    max-width: 400px;
+                    text-align: center;
+                    margin: 0 auto;
+                    padding: 40px;
+                    border-radius: 8px;
+                    border: 1px solid #f9f9f9;
+                    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 16px;
+                    }
+
+                    input[type=file], .image-preview {
+                    display: block;
+                    margin: 20px 5px;
+                    }
+
+                    .image-preview {
+                    border-radius: 8px;
+                    max-width: 200px;
+                    max-height: 400px;
+                    }
+                    .name {
+                        margin-top: 40px;
+                        text-align: center;
+                      }
+                `}</style>
+            </>
+        ) : (
+            <>
+                <Loading />
+            </>
+        )}
+    </div>
+  )
 }
