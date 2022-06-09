0ea7ac6c05b3b113571b0e85b4fbf3e2f1bcb259
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-06-09 15:20:35 -0500

    changed image

--- components/Layout.js
+++ components/Layout.js
@@ -14,13 +14,13 @@ const Layout = (props) => (
       <meta property="og:url" content="https://oustro.xyz" />
       <meta property="og:title" content="Oustro" />
       <meta property="og:description" content="Welcome to the future of Publishing" />
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="og:image" content="https://i.imgur.com/WFtldTw.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://oustro.xyz" />
       <meta property="twitter:title" content="Oustro" />
       <meta property="twitter:description" content="Welcome to the future of Publishing" />
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <meta property="twitter:image" content="https://i.imgur.com/WFtldTw.png" />
 
     </Head>
     <Header />
