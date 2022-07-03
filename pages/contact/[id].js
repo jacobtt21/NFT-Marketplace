fcd194924a366c6acbb2775e1a4edfbcaed37ecc
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-07-03 01:07:31 -0500

    updated contact page

--- pages/contact/[id].js
+++ pages/contact/[id].js
@@ -1,6 +1,6 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useCallback } from 'react';
 import { UserContext } from '../../lib/UserContext';
-import { TextField, CallToAction } from '@magiclabs/ui';
+import { TextField, CallToAction, ComboBox , Item, useToast} from '@magiclabs/ui';
 import Loading from '../../components/Loading';
 import { useRouter } from 'next/router'
 
@@ -9,20 +9,27 @@ function Contact() {
     const [message, setMessage] = useState('');
     const [sending, setSending] = useState(false);
     const getformURL = "https://getform.io/f/75702bd8-ad85-44d1-bfcf-0b3cea026d82"
+    const { createToast } = useToast();
     const router = useRouter();
 
     const formSubmit = async () => {
+        if (message === "") {
+            createToast({
+                message: 'Missing Required Fields',
+                type: 'error',
+                lifespan: 2000,
+            });
+            return;
+        }
         setSending(true)
         const formData = new FormData();
-        formData.append("email", user.email);
-        formData.append("ID", router.query.id);
+        formData.append("Reporting ID", router.query.id);
         formData.append("message", message);
         await fetch(getformURL, {
             method: "POST",
             body: formData
         });
-        setMessage('');
-        setSending(false)
+        router.reload(window.location.pathname);
     };
 
     return (
@@ -30,24 +37,22 @@ function Contact() {
             {user ? (
                 <div>
                    <h1>Report a Work</h1>
-                   <h2>Please fill out this form if you see something that doesn't adhere to Oustro's rules</h2>
+                   <h2>We are commited to keeping Oustro a safe space and we count on you to help us to that</h2>
                    <div className='class-container'>
-                        <div className='nname'>
-                            <p>All Reports are Investigated</p>
-                        </div>
-                        <TextField
-                        placeholder='Why are you reporting this Work?'
-                        className='area'
-                        value={message}
+                        <textarea
+                        aria-label="send email"
+                        placeholder='Why are you reporting this work? Please be as detailed as possible, this will allow us
+                        to respond as fast as possible.'
                         onChange={(e) => setMessage(e.target.value)}
-                        > 
-                        </TextField>
+                        value={message}
+                        >
+                        </textarea>
                         <br />
                         <br />
                         <CallToAction
                         onClick={formSubmit}
                         >
-                            Send Report
+                            Send
                         </CallToAction>
                         <br />
                         <br />
@@ -75,16 +80,36 @@ function Contact() {
                 }
                 .class-container {
                     max-width: 400px;
-                    text-align: center;
                     margin: 0 auto;
                     padding: 40px;
                     border-radius: 8px;
                     border: 1px solid #f9f9f9;
                     box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 16px;
+                    text-align: center;
                 }
-                .nname {
-                    font-weight: bold;
-                    margin: 15px 15px;
+                textarea {
+                    margin-top: 15px;
+                    min-width: 380px;
+                    min-height: 150px;
+                    border-radius: 10px;
+                    resize: none;
+                    outline: none;
+                    font-family: Verdana;
+                    font-size: 15px;
+                    border: 1px solid;
+                    transition: 0.2s;
+                    border: 1px solid #E5E5E5;
+                    padding: 10px;
+                }
+                textarea:hover {
+                    outline: none !important;
+                    border: 1px solid #6851FF;
+                }
+                textarea:focus {
+                    transition: 0.2s;
+                    outline: none !important;
+                    border: 1px solid #6851FF;
+                    box-shadow: 0 0 1px 2px #6851FF; 
                 }
             `}</style>
         </div>
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,25 +1,37 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useCallback } from 'react';
 import { UserContext } from '../../lib/UserContext';
-import { TextField, CallToAction } from '@magiclabs/ui';
+import { TextField, CallToAction, ComboBox , Item, useToast} from '@magiclabs/ui';
 import Loading from '../../components/Loading';
+import { useRouter } from 'next/router'
 
 function Contact() {
     const [user] = useContext(UserContext);
     const [message, setMessage] = useState('');
+    const [name, setName] = useState('');
     const [sending, setSending] = useState(false);
     const getformURL = "https://getform.io/f/75702bd8-ad85-44d1-bfcf-0b3cea026d82"
+    const { createToast } = useToast();
+    const router = useRouter();
 
     const formSubmit = async () => {
+        if (name === "" || message === "") {
+            createToast({
+                message: 'Missing Required Fields',
+                type: 'error',
+                lifespan: 2000,
+            });
+            return;
+        }
         setSending(true)
         const formData = new FormData();
         formData.append("email", user.email);
         formData.append("message", message);
+        formData.append("name", name);
         await fetch(getformURL, {
             method: "POST",
             body: formData
         });
-        setMessage('');
-        setSending(false)
+        router.reload(window.location.pathname);
     };
 
     return (
@@ -29,16 +41,35 @@ function Contact() {
                    <h1>Contact Us</h1>
                    <h2>We read everything you send because we care about the experience you have on Oustro</h2>
                    <div className='class-container'>
-                        <div className='nname'>
-                            <p>Write your message here, don't worry we read everything</p>
-                        </div>
-                        <TextField
-                        placeholder='Write here'
-                        className='area'
-                        value={message}
+                       <TextField
+                       placeholder='Name'
+                       onChange={(e) => setName(e.target.value)}
+                       >
+                       </TextField>
+                       <br />
+                       <ComboBox
+                        defaultInputValue=""
+                        menuTrigger="focus"
+                        showMenuButton
+                        size="md"
+                        >
+                            <Item textValue="Press & PR">
+                                Press & PR
+                            </Item>
+                            <Item textValue="Customer Support">
+                                Customer Support
+                            </Item>
+                            <Item textValue="Feedback">
+                                Feedback
+                            </Item>
+                        </ComboBox>
+                        <textarea
+                        aria-label="send email"
+                        placeholder='What do you want to tell us? Please be as detailed as possible.'
                         onChange={(e) => setMessage(e.target.value)}
-                        > 
-                        </TextField>
+                        value={message}
+                        >
+                        </textarea>
                         <br />
                         <br />
                         <CallToAction
@@ -72,16 +103,36 @@ function Contact() {
                 }
                 .class-container {
                     max-width: 400px;
-                    text-align: center;
                     margin: 0 auto;
                     padding: 40px;
                     border-radius: 8px;
                     border: 1px solid #f9f9f9;
                     box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 16px;
+                    text-align: center;
                 }
-                .nname {
-                    font-weight: bold;
-                    margin: 15px 15px;
+                textarea {
+                    margin-top: 15px;
+                    min-width: 380px;
+                    min-height: 150px;
+                    border-radius: 10px;
+                    resize: none;
+                    outline: none;
+                    font-family: Verdana;
+                    font-size: 15px;
+                    border: 1px solid;
+                    transition: 0.2s;
+                    border: 1px solid #E5E5E5;
+                    padding: 10px;
+                }
+                textarea:hover {
+                    outline: none !important;
+                    border: 1px solid #6851FF;
+                }
+                textarea:focus {
+                    transition: 0.2s;
+                    outline: none !important;
+                    border: 1px solid #6851FF;
+                    box-shadow: 0 0 1px 2px #6851FF; 
                 }
             `}</style>
         </div>
