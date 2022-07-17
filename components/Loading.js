b2faea7436fcad09882e91c4ba09322c032952cf
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-07-17 15:51:22 -0500

    new loading

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <img src="/loading.gif" height={120} />
+      <img src="/loading1.gif" height={120} />
     </div>
   );
 }
