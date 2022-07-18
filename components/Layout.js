27b92495c9823271e8cf64370c2e5245b039d8ad
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-07-18 16:32:23 -0500

    changed image

--- components/Layout.js
+++ components/Layout.js
@@ -9,19 +9,19 @@ const Layout = (props) => (
       <link rel="icon" href="/favicon.ico" />
 
       <meta name="title" content="Oustro" />
-      <meta name="description" content="Publishing has never been easier and supporting creators has never been so rewarding. Welcome to Oustro." />
+      <meta name="description" content="We all have a story, tell yours" />
 
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.oustro.xyz" />
+      <meta property="og:url" content="https://www.oustro.xyz/showcase" />
       <meta property="og:title" content="Oustro" />
-      <meta property="og:description" content="Publishing has never been easier and supporting creators has never been so rewarding. Welcome to Oustro." />
-      <meta property="og:image" content="https://i.imgur.com/BXM0bAb.png" />
+      <meta property="og:description" content="We all have a story, tell yours" />
+      <meta property="og:image" content="https://i.imgur.com/TKVuIhF.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.oustro.xyz" />
+      <meta property="twitter:url" content="https://www.oustro.xyz/showcase" />
       <meta property="twitter:title" content="Oustro" />
-      <meta property="twitter:description" content="Publishing has never been easier and supporting creators has never been so rewarding. Welcome to Oustro." />
-      <meta property="twitter:image" content="https://i.imgur.com/BXM0bAb.png" />
+      <meta property="twitter:description" content="We all have a story, tell yours" />
+      <meta property="twitter:image" content="https://i.imgur.com/TKVuIhF.png" />
     </Head>
     <Header />
     <main>
