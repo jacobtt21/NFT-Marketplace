c84a8e9e96bbd369699e263ff27d1bbc6500650b
Jacob Thomas <jacobthomas@Jacob-Thomas-MacBook-Pro.local>
2022-08-13 23:22:02 -0500

    back to basics

--- pages/login.js
+++ pages/login.js
@@ -21,7 +21,7 @@ export default function Login() {
 
     try {
       // Grab auth token after user clicks magic link in email
-      const didToken = await magic.auth.loginWithMagicLink({ email, redirectURI: 'https://www.oustro.xyz/fyp' });
+      const didToken = await magic.auth.loginWithMagicLink({ email });
 
       // Validate auth token with server
       const res = await fetch('/api/login', {
